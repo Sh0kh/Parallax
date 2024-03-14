a69b1bbdd941e908843ae38b0029add59b5205d3
Sh0kh <shohruuh07tuxtanazarov@gmail.com>
2024-03-14 06:58:12 +0500

    if

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bush2 = document.querySelector('.bush2')
 const bush1 = document.querySelector('.bush1')
 const mount2 = document.querySelector('.mount2')
 const mount1 = document.querySelector('.mount1')
+const aboutContent = document.querySelector('.about-content')
+const aboutImg = document.querySelector('.about-img')
 
 document.addEventListener('scroll', function(){
     let value = window.scrollY
@@ -14,4 +16,19 @@ document.addEventListener('scroll', function(){
     bush2.style.marginBottom = -value + 'px'
     mount1.style.marginBottom = -value *1.1 + 'px'
     mount2.style.marginBottom = -value *1.2 + 'px'
+    aboutContent.style.marginLeft = value + 'px'
+    let img = aboutImg.style.marginRight = value + "px"
+    console.log(value);
 })
+const heading = document.getElementById('heading');
+
+window.onscroll = function(img2) {
+  const scrollTop = window.pageYOffset;
+  
+  if (scrollTop > 860) {
+    heading.style.color = 'blue';
+   img = aboutImg.style.marginLeft = value*1.2 + "px"
+  } else {
+    heading.style.color = 'red';
+  }
+};
